cac4a43f9576232db5f4aded98a04656db43f1b6
Jorge Espinosa Lara <joespla2000@gmail.com>
2020-04-05 22:19:55 -0500

    User moviemnt added. 'Enter move method updated

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -15,7 +15,32 @@ def EnterMove(board):
     #
     # La función acepta el estaduo actual del trablero y pregunta al usuario de su movimiento,
     # verifica la entrada y actualiza el tablero acorde a la decisión del usuario
-    return None
+    boardUpdate = board[:]
+    #verification = True
+    print("Make a move. Tell me where you want to move: ")
+    userMovement = int(input())
+    
+    # Esto va a checar que el movimiento del usuario sea valida
+    '''
+    if userMovement not in boardUpdate:
+        verification = False
+        
+        # Checa que el tiro del usuario sea valido
+        while not verification:
+            print ("Please make a valid move.")
+            userMovement = int(input())
+            if userMovement in boardUpdate:
+                verification = True
+    '''
+
+    # Busca la posicion dada por el usuario y la ingresa en el tablero    
+    for line in range(3):
+        for column in range(3):
+            if boardUpdate[line][column] == userMovement:
+                boardUpdate[line][column] = 'O'
+                break
+            
+    return boardUpdate
 
 
 def MakeListOfFreeFields(board):
@@ -43,4 +68,15 @@ def DrawMove(board):
 
 
 board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+auxBoard = []
+startGame = True
+
+while startGame:
+    auxBoard = EnterMove(board)
+    del (board[:])
+    board = auxBoard[:]
+    del (auxBoard[:])
+    DisplayBoard(board)
+    
+
 
